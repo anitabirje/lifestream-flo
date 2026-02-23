4c65f3595b93438e2c0ff91511746ecb7b9ab073
Anita Birje <anita.birje@gmail.com>
2026-02-23 18:12:05 +0800

    fix: git ignore, include package-lock and generate diagram script

--- packages/frontend/dev-dist/sw.js
+++ packages/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ba5c9bb8'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r1nugatbg6g"
+    "revision": "0.7lahehnvu8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
